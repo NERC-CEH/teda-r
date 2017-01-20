e4020a4a81ea6e79b2e3fe9feafbfa0cc6cf9280
David Ciar <dciar86@ceh.ac.uk>
2017-01-20 16:07:02 +0000

    Updated batch method + maintenance version bump
    - Modified the batch method to only work with vector observation objects, any other causes
    a NA filled teda batch object to be returned.
    - Version bump to represent the modifications in this and previous commit

--- R/teda-batch.R
+++ R/teda-batch.R
@@ -37,48 +37,60 @@
 #' @export
 teda_b = function(observations, dist_type = "Euclidean"){
 
-  # Accumulated Proximity: The sum of the distances
-  #   between all observation points in both directions
-  spi = sum(as.matrix(dist(observations,diag = TRUE, upper = TRUE)))
-
-  # Distance between point of interest and all other points,
-  #   create the matrix, then use the index to lookup the
-  #   row of interest.  `upper` is not set to true to match
-  #   the paper equations - if it was set to true, the 2*
-  #   operation could be removed from the eccentricity
-  #   calculation
-  distance_matrix = as.matrix(dist(observations,
-                                   diag = TRUE))
-
-  # Identify this length of the observation_vector, as it
-  #   may be a vector or matrix, with different accessors
+  # Identify the type of the observations
   if (is.vector(observations)) {
+
+    # Accumulated Proximity: The sum of the distances
+    #   between all observation points in both directions
+    spi = sum(as.matrix(dist(observations,diag = TRUE, upper = TRUE)))
+
+    # Distance between point of interest and all other points,
+    #   create the matrix.  `upper` is not set to true to match
+    #   the paper equations - if it was set to true, the 2*
+    #   operation could be removed from the eccentricity
+    #   calculation
+    distance_matrix = as.matrix(dist(observations,
+                                     diag = TRUE))
+
+    # Eccentricity: the relative (normalised) accumulated
+    #   proximity of that data sample as a fraction of the
+    #   accumulated proximity of all other data samples.
+    #
+    # Calculate eccentricity for each position
+    ecc = unname((2 * apply(distance_matrix, 1, sum)) / spi)
+
+    # Record the vector length for normalisation and outlier
+    #  identification methods
     obs_len = length(observations)
+
+    # Create the return object holding all the required output,
+    #   and set the class for use with generic functions
+    ret_obj = c()
+    ret_obj$observations = observations
+    ret_obj$eccentricity = round(ecc,3)
+    ret_obj$typicality = round(1 - ecc,3)
+    ret_obj$norm_eccentricity = round(ecc / 2,3)
+    ret_obj$norm_typicality = round(ret_obj$typicality / (obs_len - 2), 3)
+    ret_obj$outlier = ret_obj$norm_eccentricity > (1 / obs_len)
+    ret_obj$ecc_threshold = round(1 / obs_len, 3)
+    class(ret_obj) = "tedab"
+
+    return(ret_obj)
+
   }else{
-    obs_len = dim(observations)[1]
+
+    # If a matrix or dataframe is past to the function, set the
+    #  return object positions to be NA
+    ret_obj = c()
+    ret_obj$observations = NA
+    ret_obj$eccentricity = NA
+    ret_obj$typicality = NA
+    ret_obj$norm_eccentricity = NA
+    ret_obj$norm_typicality = NA
+    ret_obj$outlier = NA
+    ret_obj$ecc_threshold = NA
+    class(ret_obj) = "tedab"
+
+    return(ret_obj)
   }
-
-  # NB: Only vector implementation, multivariate currently
-  #  not supported
-
-  # Eccentricity: the relative (normalised) accumulated
-  #   proximity of that data sample as a fraction of the
-  #   accumulated proximity of all other data samples.
-  #
-  # Calculate eccentricity for each position
-  ecc = unname((2 * apply(distance_matrix, 1, sum)) / spi)
-
-  # Create the return object holding all the required output,
-  #   and set the class for use with generic functions
-  ret_obj = c()
-  ret_obj$observations = observations
-  ret_obj$eccentricity = round(ecc,3)
-  ret_obj$typicality = round(1 - ecc,3)
-  ret_obj$norm_eccentricity = round(ecc / 2,3)
-  ret_obj$norm_typicality = round(ret_obj$typicality / (obs_len - 2), 3)
-  ret_obj$outlier = ret_obj$norm_eccentricity > (1 / obs_len)
-  ret_obj$ecc_threshold = round(1 / obs_len, 3)
-  class(ret_obj) = "tedab"
-
-  return(ret_obj)
 }
